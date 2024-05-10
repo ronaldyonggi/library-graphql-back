cc91b5ac2af33fd4f59a0350427f8f06d5c753fe
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-10 17:32:06 +0800

    8.7 implement mutation editAuthor to edit author's birth year

--- index.js
+++ index.js
@@ -91,8 +91,9 @@ const typeDefs = `
   
   type Author {
     name: String!
-    id: ID!
     bookCount: Int!
+    born: Int
+    id: ID!
   }
 
   type Query {
@@ -109,6 +110,11 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+
+    editAuthor (
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `;
 
@@ -164,6 +170,16 @@ const resolvers = {
 
       books = books.concat(newBook);
       return newBook;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };

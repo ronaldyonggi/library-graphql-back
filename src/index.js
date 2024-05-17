6aee565832873566206e84e2c8e45f4468088f88
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-17 15:12:47 +0800

    8.14 configure MongoDB connection and PORT number

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
-const { typeDefs } = require('./graphql/schema')
-const { resolvers } = require('./graphql/resolvers')
+const { typeDefs } = require('./graphql/schema');
+const { resolvers } = require('./graphql/resolvers');
+const { PORT } = require('./utils/config');
+const { connectToDatabase } = require('./utils/db');
+
+connectToDatabase();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: PORT },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });

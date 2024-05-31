bbd6e357f9cea68acf166fb5c9910a8f9322fad1
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-31 14:25:11 +0800

    removed startStandAloneServer import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
 const { typeDefs } = require('./graphql/schema');
 const { resolvers } = require('./graphql/resolvers');
 const { PORT } = require('./utils/config');

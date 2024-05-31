7598c253c9ba9f1ba6548d5bf2e383ad93d5378e
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-31 14:20:33 +0800

    8.23 refactor server to use expressMiddleware

--- src/index.js
+++ src/index.js
@@ -5,26 +5,53 @@ const { resolvers } = require('./graphql/resolvers');
 const { PORT } = require('./utils/config');
 const { connectToDatabase } = require('./utils/db');
 const jwt = require('jsonwebtoken');
+
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { expressMiddleware } = require('@apollo/server/express4');
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer');
+
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
 const { JWT_SECRET } = require('./utils/config');
 const User = require('./models/user');
 
 connectToDatabase();
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+// Setup is now within a function
 
-startStandaloneServer(server, {
-  listen: { port: PORT },
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null;
-    if (auth && auth.startsWith('Bearer ')) {
-      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
-      const currentUser = await User.findById(decodedToken.id);
-      return { currentUser };
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req, res }) => {
+        const auth = req ? req.headers.authorization : null;
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+          const currentUser = await User.findById(decodedToken.id);
+          return { currentUser };
+        }
+      },
+    })
+  );
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server is now running on http://localhost:${PORT}`);
+  });
+};
+
+start();
